15596d11c8ec2ac17c2dbe32f711ac585a12a5da
Vetal <webliga@ukr.net>
2013-09-22 21:44:48 +0300

    1

--- application/core/Core.php
+++ application/core/Core.php
@@ -9,7 +9,7 @@ class Core
     private $_secure; // Объект класса Secure   
     private $_grammatical; // Объект класса Grammatical   
     private $_request; // Объект класса Request    
-    private $_route; // Объект класса Route    333
+    private $_route; // Объект класса Route    1
 
     private function __construct()
     {
