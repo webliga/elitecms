cdcf9c69acf269a4b43d03cce1aac0a2bf9620b7
Vetal <webliga@ukr.net>
2013-09-23 03:07:41 +0300

    Добавлен базовый  класс пользователя

---
+++ application/core/User.php
@@ -0,0 +1,16 @@
+<?php
+
+class User extends Base
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    function __destruct()
+    {
+        parent::__destruct();
+    }
+}
+
+?>
