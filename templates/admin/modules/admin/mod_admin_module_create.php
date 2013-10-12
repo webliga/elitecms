955d2f2bea2aa10d23e05df1f34078f694983174
Vetal <webliga@ukr.net>
2013-10-12 15:27:24 +0300

    Модуль админка 5

--- templates/admin/modules/admin/mod_admin_module_create.php
+++ templates/admin/modules/admin/mod_admin_module_create.php
@@ -1,8 +1,9 @@
 <div class="mod_create">
-    <form class="form-horizontal">
+    <form class="form-horizontal" action="<?php echo  Core::app()->getHtml()->createUrl('admin/modules/update/');?>" method="post">
         <div class="control-group">
             <label class="control-label" for="inputEmail">Название модуля:</label>
             <div class="controls">
+                <input type="hidden" name="id_module" value="<?php echo $dataArr['id']; ?>">
                 <input type="text"  class="form-control" value="<?php echo $dataArr['name']; ?>"  name="name">
             </div>
         </div>
